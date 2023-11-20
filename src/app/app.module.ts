1e07ca58e56d54a05b821985987da149d522c425
Javier Arenas <javier.arenas@vipera.com>
2023-11-20 18:03:27 +0100

    feat: BEM CSS & Internationalization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,36 @@ import {AppRoutingModule} from './app-routing.module';
 import {HomePage} from "./pages/home/home.page";
 import {CdkFixedSizeVirtualScroll, CdkVirtualForOf, CdkVirtualScrollViewport} from "@angular/cdk/scrolling";
 import {FormsModule} from "@angular/forms";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePage,
   ],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, CdkVirtualScrollViewport, FormsModule, CdkFixedSizeVirtualScroll, CdkVirtualForOf],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    AppRoutingModule,
+    CdkVirtualScrollViewport,
+    FormsModule,
+    CdkFixedSizeVirtualScroll,
+    CdkVirtualForOf
+  ],
   providers: [{provide: RouteReuseStrategy, useClass: IonicRouteStrategy}],
   bootstrap: [AppComponent],
 })
